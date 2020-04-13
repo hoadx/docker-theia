2425e89aad4ec75a0c32c72da3a7f57d11868473
Hoa Duong <duongxuanhoa@gmail.com>
2020-04-14 03:37:24 +0700

    Fix permission issue

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,14 +10,18 @@ then
         ORIG_UID=$(echo $ORIGPASSWD | cut -f3 -d:)
         ORIG_GID=$(echo $ORIGPASSWD | cut -f4 -d:)
                                                                                 
-        if [ "$USERID" != "$ORIG_UID" ] || [ "$GROUPID" != "$ORIG_GID" ]; then
+        if [ $USERID -ne $ORIG_UID ] || [ $GROUPID -ne $ORIG_GID ] \
+        || [ $USERID -ne  $(ls -rn /workspace | awk 'NR==2 {print $3 }') ] \
+        || [ $GROUPID -ne  $(ls -rn /workspace | awk 'NR==2 {print $4 }') ]
+        then
                 sed -i -e "s/:$ORIG_UID:$ORIG_GID:/:$USERID:$GROUPID:/" /etc/passwd
                 sed -i -e "s/theia:x:$ORIG_GID:/theia:x:$GROUPID:/" /etc/group          
                 ORIG_HOME=$(echo $ORIGPASSWD | cut -f6 -d:)                
                 chown -R theia:theia ${ORIG_HOME}
-                if test $CHANGE_OWNER -gt 0
+                
+                if [ $CHANGE_OWNER -gt 0 ]
                 then 
-                        chown -R theia:theia $WORKSPACE
+                        chown -R theia:theia /workspace
                 fi
         fi                                                  
 fi
